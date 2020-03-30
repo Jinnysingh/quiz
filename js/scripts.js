498889ce51072c25c2537e50d620d85b7536e932
Jaya Singh <I0819@IT001232.local>
2020-03-31 01:11:03 +0530

    feat some changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 
-let results = []
-
 function getQuizData() {
     let someVarName = JSON.parse(localStorage.getItem("question"))
     if (someVarName != null && someVarName <= 4) {
@@ -13,51 +11,54 @@ function getQuizData() {
     }
 }
 
-function selectedOptions(event){
+function selectedOptions(event) {
     let questionNum = JSON.parse(localStorage.getItem("question"))
-    if (event.target.value && event.target.value !== "" || event.target.value!== null  )
-    localStorage.setItem(questionNum, event.target.value);
-    results[questionNum] = event.target.value
-    document.getElementById("next-btn").disabled = false
+    if (event.target.value && event.target.value !== "" || event.target.value !== null){
+        localStorage.setItem(questionNum, event.target.value);
+        document.getElementById("next-btn").disabled = false
+    }
+    
 }
 
 
 function next() {
     let someVarName = JSON.parse(localStorage.getItem("question"))
     let selectedValue = localStorage.getItem(someVarName)
-    let checkvalue = localStorage.setItem("question", someVarName + 1);
-    checkvalue = JSON.parse(localStorage.getItem("question"))
+    let checkvalue = someVarName + 1;
+    localStorage.setItem("question", checkvalue);
     if (checkvalue <= 4 && selectedValue !== null) {
         document.getElementById("q" + someVarName).style.display = 'none';
         document.getElementById("q" + checkvalue).style.display = 'block';
         document.getElementById("prev-btn").disabled = false
         document.getElementById("prev-btn").style.display = "initial"
-        document.getElementById("next-btn").disabled = true
-    } else{
+    } else if (checkvalue == 5) {
+        let finalValues = []
+        for (let i = 0; i <= 4; i++) {
+            let getVal = localStorage.getItem(i)
+            finalValues.push(getVal)
+        }
         let button = document.getElementsByClassName('footer-buttons')
         button[0].style.display = 'none'
         document.getElementById("q" + someVarName).style.display = 'none';
-        document.getElementById("results").innerHTML = '<h3>Your Submitted Answer: <div> ' + results + ' </div></h3>';
-    }
-}   
-
-
-function previous() {
-    let someVarName = JSON.parse(localStorage.getItem("question"))
-    document.getElementById("q" + someVarName).style.display = 'none';
-    localStorage.setItem("question", someVarName - 1);
-    let checkvalue = JSON.parse(localStorage.getItem("question"))
-    let selectedValue = localStorage.getItem(checkvalue)
-    if (checkvalue >= 0) {
-        document.getElementById("q" + someVarName).style.display = 'none';
-        document.getElementById("q" + checkvalue).style.display = 'block';
-        document.getElementById("prev-btn").disabled = true
-        if (checkvalue == 0) {
-            document.getElementById("prev-btn").style.display = 'none'
-        }
-    }if(selectedValue !== null || "" ){
-        document.getElementById("next-btn").disabled = false
+        document.getElementById("results").innerHTML = '<h3>Your Submitted Answer: <div> ' + finalValues + ' </div></h3>';
     }
 }
 
 
+function previous() {
+    let someVarName = JSON.parse(localStorage.getItem("question"))
+    let checkvalue = someVarName - 1;
+    document.getElementById("q" + someVarName).style.display = 'none';
+    localStorage.setItem("question", checkvalue);
+    let selectedValue = localStorage.getItem(checkvalue)
+    if (checkvalue >= 0) {
+        document.getElementById("q" + someVarName).style.display = 'none';
+        document.getElementById("q" + checkvalue).style.display = 'block';
+        if (checkvalue == 0) {
+            document.getElementById("prev-btn").style.display = 'none'
+        }
+    }
+    if (selectedValue !== null) {
+        document.getElementById("next-btn").disabled = false
+    }
+}
