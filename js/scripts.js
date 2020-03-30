043e7ad970f77eb99ea8306f30dc510a887f1cbc
Jaya Singh <I0819@nsg-static-044.232.72.182.airtel.in>
2020-03-30 23:28:30 +0530

    feat changes in quize project

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,62 @@
 
-let showing = [1, 0, 0, 0, 0];
-let questions = ['q0', 'q1', 'q2', 'q3', 'q4'];
+let results = []
 
-function next() {
-    let qElems = [];
-    let results = [];
-
-    let q0 = document.forms["quizForm"]["q1"].value;
-    let q1 = document.forms["quizForm"]["q2"].value;
-    let q2 = document.forms["quizForm"]["q3"].value;
-    let q3 = document.forms["quizForm"]["q4"].value;
-    let q4 = document.forms["quizForm"]["q5"].value;
-
-    document.getElementById('prev-btn').disabled = false
-    for (var i = 0; i < questions.length; i++) {
-        qElems.push(document.getElementById(questions[i]));
+function getQuizData() {
+    let someVarName = JSON.parse(localStorage.getItem("question"))
+    if (someVarName != null && someVarName <= 4) {
+        document.getElementById("q" + someVarName).style.display = 'block';
     }
-    for (var i = 0; i < showing.length; i++) {
-        if (eval("q" + i) !== "") {
-            results.push(eval("q" + i))
-            if (showing[i] == 1) {
-                qElems[i].style.display = 'none';
-                showing[i] = 0;
-                if (i == showing.length - 1) {
-                    let button = document.getElementsByClassName('footer-buttons')
-                    button[0].style.display = 'none'
-                    document.getElementById("results").innerHTML = '<h3>Your Submitted Answer: <div> ' + results + ' </div></h3>';
-                } else {
-                    qElems[i + 1].style.display = 'block';
-                    showing[i + 1] = 1;
-                }
-                break;
-            }
-        }
-
-
+    else {
+        localStorage.setItem("question", 0);
+        document.getElementById("q0").style.display = 'block';
+        document.getElementById("prev-btn").style.display = 'none'
     }
 }
 
+function selectedOptions(event){
+    let questionNum = JSON.parse(localStorage.getItem("question"))
+    if (event.target.value && event.target.value !== "" || event.target.value!== null  )
+    localStorage.setItem(questionNum, event.target.value);
+    results[questionNum] = event.target.value
+    document.getElementById("next-btn").disabled = false
+}
+
+
+function next() {
+    let someVarName = JSON.parse(localStorage.getItem("question"))
+    let selectedValue = localStorage.getItem(someVarName)
+    let checkvalue = localStorage.setItem("question", someVarName + 1);
+    checkvalue = JSON.parse(localStorage.getItem("question"))
+    if (checkvalue <= 4 && selectedValue !== null) {
+        document.getElementById("q" + someVarName).style.display = 'none';
+        document.getElementById("q" + checkvalue).style.display = 'block';
+        document.getElementById("prev-btn").disabled = false
+        document.getElementById("prev-btn").style.display = "initial"
+        document.getElementById("next-btn").disabled = true
+    } else{
+        let button = document.getElementsByClassName('footer-buttons')
+        button[0].style.display = 'none'
+        document.getElementById("q" + someVarName).style.display = 'none';
+        document.getElementById("results").innerHTML = '<h3>Your Submitted Answer: <div> ' + results + ' </div></h3>';
+    }
+}   
+
 
 function previous() {
-    let qElems = [];
-    for (var i = 0; i < questions.length; i++) {
-        qElems.push(document.getElementById(questions[i]));
-    }
-    for (var i = 0; i < showing.length; i++) {
-        if (showing[i] == 1) {
-            qElems[i].style.display = 'none';
-            showing[i] = 0;
-            if (4 == showing.length - i) {
-                qElems[i - 1].style.display = 'block';
-                showing[i - 1] = 1;
-                document.getElementById('prev-btn').disabled = true
-            } else {
-                qElems[i - 1].style.display = 'block';
-                showing[i - 1] = 1;
-            }
-            break;
+    let someVarName = JSON.parse(localStorage.getItem("question"))
+    document.getElementById("q" + someVarName).style.display = 'none';
+    localStorage.setItem("question", someVarName - 1);
+    let checkvalue = JSON.parse(localStorage.getItem("question"))
+    let selectedValue = localStorage.getItem(checkvalue)
+    if (checkvalue >= 0) {
+        document.getElementById("q" + someVarName).style.display = 'none';
+        document.getElementById("q" + checkvalue).style.display = 'block';
+        document.getElementById("prev-btn").disabled = true
+        if (checkvalue == 0) {
+            document.getElementById("prev-btn").style.display = 'none'
         }
+    }if(selectedValue !== null || "" ){
+        document.getElementById("next-btn").disabled = false
     }
 }
 
